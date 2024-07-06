45e1494700d14f5ace3daf01f23548fac077df3b
MisABU <maashaainn@gmail.com>
2024-07-06 16:04:59 +0300

    ver 3_last test image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => (
         <hr />
         <div className="footer">
             <a href="https://t.me/abu_blood"><Image src="/frontend/images/tg_img.png" alt="Telegram" width={40} height={50}/></a>
-            <a href="https://github.com/MisABU148"><Image src="/frontend/images/gh_img" alt="GitHub" width={40} height={50}/></a>
+            <a href="https://github.com/MisABU148"><Image src="/frontend/images/gh_img.png" alt="GitHub" width={40} height={50}/></a>
             <a href="mailto:maashaainn@gmail.com"><Image src="/frontend/images/gmail_png.png" alt="Gmail" width={40} height={50}/></a>
             <a href="https://innopolis.university/"><p>&copy; m.ilyina@innopolis.university</p></a>
         </div>
